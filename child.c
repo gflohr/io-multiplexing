4750c8a4bf10da8de927f3c4bb45891a13481bad
Guido Flohr <guido.flohr@cantanea.com>
2023-08-28 12:07:25 +0300

    implement child output in C

--- child.c
+++ child.c
@@ -1,5 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
 int
 main(int argc, char *argv[])
 {
+	pid_t pid = getpid();
+
+	/* Make standard output and standard error line buffered.  */
+	setvbuf(stdout, NULL, _IOLBF, 0);
+	setvbuf(stderr, NULL, _IOLBF, 0);
+
+	srand(time(NULL));
+
+	useconds_t us;
+
+	while (1) {
+		us = rand() % 3000000;
+		fprintf(stdout,
+			"child pid %u writing to stdout before sleeping %u μs.\n",
+			pid, us);
+		usleep(us);
+	}
+
 	return 0;
 }
--- child.pl
+++ child.pl
@@ -3,7 +3,6 @@
 # ABSTRACT: Platform-independent example for IO multiplexing
 
 use strict;
-use v5.10;
 
 use Time::HiRes qw(usleep);
 
@@ -14,9 +13,9 @@ my $us;
 
 while (1) {
 	$us = int rand 3_000_000;
-	say STDOUT "child pid $$ writing to stdout before sleeping $us μs.";
+	print STDOUT "child pid $$ writing to stdout before sleeping $us μs.\n";
 	usleep $us;
 	$us = int rand 3_000_000;
-	say STDOUT "child pid $$ writing to stderr before sleeping $us μs.";
+	print STDOUT "child pid $$ writing to stderr before sleeping $us μs.\n";
 	usleep $us;
 }
